2cac7a8aadac5293b7baa27e034fb45aee0414ea
nick <nikhil@krishagni.com>
2020-04-24 13:55:57 +0530

    Delete the item from the collection of mongoDb database and create new item and reflect in webpage without reloading the page

---
+++ todo-app/public/browser.js
@@ -0,0 +1,36 @@
+function itemTemplate(item) {
+    return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+    <span class="item-text">${item.text}</span>
+    <div>
+    <button data-id="${item._id}"class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+    <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
+    </div>
+    </li>`;
+}
+
+let creatField = document.getElementById("create-field")
+document.getElementById("create-form").addEventListener("submit", function(e) {
+    e.preventDefault()
+    axios.post("/create-item", { text: creatField.value }).then(function(response) {
+        document.getElementById("item-list").insertAdjacentHTML("beforeend", itemTemplate(response.data))
+        creatField.value = ""
+        creatField.focus()
+    }).catch(function() {
+        alert("Please try again later")
+    })
+})
+document.addEventListener("click", (e) => {
+
+    if (e.target.classList.contains("delete-me")) {
+        axios.post("delete-item", { id: e.target.getAttribute("data-id") }).then((value) => {
+            e.target.parentElement.parentElement.remove()
+        })
+    }
+
+    if (e.target.classList.contains("edit-me")) {
+        let userInput = prompt("Enter you desired text:", e.target.parentElement.parentElement.querySelector(".item-text").innerHTML)
+        axios.post("/update-item", { text: userInput, id: e.target.getAttribute("data-id") }).then((value) => {
+            e.target.parentElement.parentElement.querySelector(".item-text").innerHTML = userInput
+        })
+    }
+})
--- todo-app/server.js
+++ todo-app/server.js
@@ -2,16 +2,19 @@ let express = require('express')
 let mongodb = require("mongodb")
 let app = express()
 let db
+
+app.use(express.static("public"))
 let connectionString = "mongodb+srv://nick:nick1234@cluster0-1lqds.mongodb.net/TodoDB?retryWrites=true&w=majority"
     //let connectionString = "mongodb://localhost/TodoDB?retryWrites=true&w=majority"  //connectionstring for local mongo db
 mongodb.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true }, function(err, client) {
     db = client.db()
     app.listen(3000)
 })
+app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.get('/', (req, res) => {
             db.collection("items").find().toArray(function(err, items) {
-                        console.log(items)
+
 
                         res.send(`
           <!DOCTYPE html>
@@ -27,28 +30,30 @@ app.get('/', (req, res) => {
           <h1 class="display-4 text-center py-1">To-Do App</h1>
           
           <div class="jumbotron p-3 shadow-sm">
-            <form action="/create-item" method="POST">
+            <form id="create-form" action="/create-item" method="POST">
               <div class="d-flex align-items-center">
-                <input name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
+                <input id="create-field" name="item" autofocus autocomplete="off" class="form-control mr-3" type="text" style="flex: 1;">
                 <button class="btn btn-primary">Add New Item</button>
               </div>
             </form>
           </div>
           
-          <ul class="list-group pb-5">
+          <ul id="item-list" class="list-group pb-5">
            ${items.map(function(item){
              return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
              <span class="item-text">${item.text}</span>
              <div>
-             <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
-             <button class="delete-me btn btn-danger btn-sm">Delete</button>
+             <button data-id="${item._id}"class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+             <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
              </div>
              </li>`
             }).join(" ")}
           </ul>
           
         </div>
-        
+        <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+
+        <script src="/browser.js"></script>
       </body>
       </html>
     `)
@@ -56,7 +61,19 @@ app.get('/', (req, res) => {
 })
 
 app.post("/create-item", (req, res) => {
-    db.collection("items").insertOne({ text: req.body.item }, function() {
-        res.redirect("/")
+    db.collection("items").insertOne({ text: req.body.text }, function(err,info) {
+        res.json(info.ops[0])
     })
+})
+
+app.post('/update-item',function(req,res){
+  db.collection("items").findOneAndUpdate({_id: new mongodb.ObjectID(req.body.id)},{$set: {text: req.body.text}},function(){
+    res.send("Sucess")
+  })
+})
+
+app.post("/delete-item", function(req,res){
+  db.collection('items').deleteOne({_id: new mongodb.ObjectID(req.body.id)}, function(){
+    res.send("Sucess")
+  })
 })

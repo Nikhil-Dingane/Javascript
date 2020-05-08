09423e571260fed0b2e6ea642f8186e6574b78f2
nick <nikhil.nikhil.dingane1@gmail.com>
2020-05-08 14:26:56 +0530

    improvements in user login, user registraion, and data validations

--- complex-app/models/User.js
+++ complex-app/models/User.js
@@ -29,31 +29,49 @@ User.prototype.cleanUp = function() {
 }
 
 User.prototype.validate = function() {
-    if (this.data.username == "") {
-        this.errors.push("Use must provide a username")
-    }
-    if (this.username != "" && !validator.isAlphanumeric(this.data.username)) {
-        this.errors.push("Username consist alphabets and numbers only")
-    }
-    if (!validator.isEmail(this.data.email)) {
-        this.errors.push("Use must provide a valid email address")
-    }
-    if (this.data.password == "") {
-        this.errors.push("Use must provide a password")
-    }
+    return new Promise(async(resolve, reject) => {
+        if (this.data.username == "") {
+            this.errors.push("Use must provide a username")
+        }
+        if (this.username != "" && !validator.isAlphanumeric(this.data.username)) {
+            this.errors.push("Username consist alphabets and numbers only")
+        }
+        if (!validator.isEmail(this.data.email)) {
+            this.errors.push("Use must provide a valid email address")
+        }
+        if (this.data.password == "") {
+            this.errors.push("Use must provide a password")
+        }
 
-    if (this.data.password.length < 12) {
-        this.errors.push("Passowrd must be at least of 12 characters")
-    }
-    if (this.data.password.length > 50) {
-        this.errors.push("Passowrd must be less than 50")
-    }
-    if (this.data.username.length < 3) {
-        this.errors.push("Username must be at least of 3 characters")
-    }
-    if (this.data.username.length > 30) {
-        this.errors.push("Username must be less than 30")
-    }
+        if (this.data.password.length < 12) {
+            this.errors.push("Passowrd must be at least of 12 characters")
+        }
+        if (this.data.password.length > 50) {
+            this.errors.push("Passowrd must be less than 50")
+        }
+        if (this.data.username.length < 3) {
+            this.errors.push("Username must be at least of 3 characters")
+        }
+        if (this.data.username.length > 30) {
+            this.errors.push("Username must be less than 30")
+        }
+
+        //Only if username is valid then check to see if its already taken
+        if (this.data.username.length > 2 && this.data.username.length < 31 && validator.isAlphanumeric(this.data.username)) {
+            let usernameExists = await usersCollection.findOne({ username: this.data.username })
+            if (usernameExists) {
+                this.errors.push("The username is already taken")
+            }
+        }
+        //email
+        if (validator.isEmail(this.data.email)) {
+            let emailExists = await usersCollection.findOne({ email: this.data.email })
+            if (emailExists) {
+                this.errors.push("The email is already taken")
+            }
+        }
+        resolve()
+    })
 }
 
 User.prototype.login = function(callback) {
@@ -69,17 +87,22 @@ User.prototype.login = function(callback) {
 }
 
 User.prototype.register = function() {
-    // if #1: validate user data
-    this.validate();
-    // if #2 only if there are no validation errors
-    //then save the user data into a database
-    if (!this.errors.length) {
-        // hash user password 
-        let salt = bcrypt.genSaltSync(10)
-        this.data.password = bcrypt.hashSync(this.data.password, salt)
-        usersCollection.insertOne(this.data);
-    }
+    return new Promise(async(resolve, reject) => {
+        // if #1: validate user data
+        await this.validate();
+        // if #2 only if there are no validation errors
+        //then save the user data into a database
+        if (!this.errors.length) {
+            // hash user password 
+            let salt = bcrypt.genSaltSync(10)
+            this.data.password = bcrypt.hashSync(this.data.password, salt)
+            await usersCollection.insertOne(this.data);
+            resolve()
+        } else {
+            reject(this.errors)
+        }
 
+    })
 }
 
 module.exports = User

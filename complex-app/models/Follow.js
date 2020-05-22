afeb8c1f262005d0085b37bc3862fd7a2de1cd86
nick <nikhil.nikhil.dingane1@gmail.com>
2020-05-22 15:23:07 +0530

    Added feature of let users stop following the other followed users and added followers screen

--- complex-app/models/Follow.js
+++ complex-app/models/Follow.js
@@ -1,6 +1,7 @@
 const usersCollection = require("../db").db().collection("users")
 const followsCollection = require("../db").db().collection("follows")
 const ObjectID = require("mongodb").ObjectID
+const User = require("./User")
 
 let Follow = function(follwedUsername, authorId) {
     this.follwedUsername = follwedUsername
@@ -12,7 +13,7 @@ Follow.prototype.cleanUp = function() {
     if (typeof(this.follwedUsername) != "string") { this.follwedUsername = "" }
 }
 
-Follow.prototype.validate = async function() {
+Follow.prototype.validate = async function(action) {
     // followedUsernname must exist in database
 
     let followedAccount = await usersCollection.findOne({ username: this.follwedUsername })
@@ -22,17 +23,75 @@ Follow.prototype.validate = async function() {
     } else {
         this.errors.push("You can not follow the user that does not exist.")
     }
+    let doesFollowAlreadyExist = await followsCollection.findOne({ followedId: this.followedId, authorId: new ObjectID(this.authorId) })
+    if (action == "create") {
+        if (doesFollowAlreadyExist) { this.errors.push("You are already following this user!") }
+    }
+    if (action == "delete") {
+        if (!doesFollowAlreadyExist) { this.errors.push("You can not stop following someone you do not already follow!") }
+    }
+    // should not be able to follow yourself
+    if (this.followedId.equals(this.authorId)) { this.errors.push("You can not follow yourself") }
 }
 
 Follow.prototype.create = function() {
     return new Promise(async(resovle, reject) => {
         this.cleanUp()
-        await this.validate()
+        await this.validate("create")
         if (!this.errors.length) {
             await followsCollection.insertOne({ followedId: this.followedId, authorId: new ObjectID(this.authorId) })
             resovle()
         } else {
             reject(this.errors)
+        }
+    })
+}
+
+Follow.isVisitorFollowing = async function(followedId, visitorId) {
+    let followDoc = await followsCollection.findOne({ followedId: followedId, authorId: new ObjectID(visitorId) })
+
+    if (followDoc) {
+        return true
+    } else {
+        return false
+    }
+}
+
+Follow.prototype.delete = function() {
+    return new Promise(async(resovle, reject) => {
+        this.cleanUp()
+        await this.validate("delete")
+        if (!this.errors.length) {
+            await followsCollection.deleteOne({ followedId: this.followedId, authorId: new ObjectID(this.authorId) })
+            resovle()
+        } else {
+            reject(this.errors)
+        }
+    })
+}
+
+Follow.getFollwersById = function(id) {
+    return new Promise(async(resolve, reject) => {
+        try {
+            let followers = await followsCollection.aggregate([
+                { $match: { followedId: id } },
+                { $lookup: { from: "users", localField: "authorId", foreignField: "_id", as: "userDoc" } },
+                {
+                    $project: {
+                        username: { $arrayElemAt: ["$userDoc.username", 0] },
+                        email: { $arrayElemAt: ["$userDoc.email", 0] }
+                    }
+                }
+            ]).toArray()
+
+            followers = followers.map(function(follower) {
+                let user = new User(follower, true)
+                return { username: follower.username, avatar: user.avatar }
+            })
+            resolve(followers)
+        } catch (e) {
+            console.log(e)
+            reject()
         }
     })
 }
--- complex-app/router.js
+++ complex-app/router.js
@@ -12,7 +12,9 @@ router.post("/login", userController.login)
 router.post("/logout", userController.logout)
 
 // profile related routes
-router.get("/profile/:username", userController.ifUserExists, userController.profilePostsScreen)
+router.get("/profile/:username", userController.ifUserExists, userController.sharedProfileData, userController.profilePostsScreen)
+router.get("/profile/:username/followers", userController.ifUserExists, userController.sharedProfileData, userController.profileFollowersScreen)
+
 
 // post related routes
 router.get("/create-post", userController.mustBeLoggedIn, postController.viewCreateScreen)
@@ -25,4 +27,6 @@ router.post("/search", postController.search)
 
 //Follow related routes
 router.post('/addFollow/:username', userController.mustBeLoggedIn, followController.addFollow)
+router.post('/removeFollow/:username', userController.mustBeLoggedIn, followController.removeFollow)
+
 module.exports = router

154e68802445e2cbcf9f2ca41b6f5486ce27aeff
nick <nikhil.nikhil.dingane1@gmail.com>
2020-05-29 16:47:59 +0530

    added email and password validations on client side

--- complex-app/models/User.js
+++ complex-app/models/User.js
@@ -146,4 +146,20 @@ User.findByUserName = function(username) {
     })
 }
 
+User.doesEmailExist = function(email) {
+    return new Promise(async function(resovle, reject) {
+        if (typeof(email) != "string") {
+            resolve(false)
+            return
+        }
+
+        let user = await usersCollection.findOne({ email: email })
+        if (user) {
+            resovle(true)
+        } else {
+            resovle(false)
+        }
+    })
+}
+
 module.exports = User
